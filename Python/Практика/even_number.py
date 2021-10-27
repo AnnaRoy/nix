2e907e05ebff6371c45377a85ad51b3a19a9fe07
AnnaRoy <anna.roy2017@gmail.com>
2021-10-27 19:03:25 +0300

    Четные умнажаем на 10  не четные на  9 (изменен комментарий что бы работал help(simple_multiplication))

--- Python/Практика/even_number.py
+++ Python/Практика/even_number.py
@@ -1,4 +1,5 @@
-'''Четные умнажаем на 10  не четные на  9'''
-
 def simple_multiplication(number):
-    return  number * 10 if number % 2 == 0 else number * 9
+    '''Четные умнажаем на 10  не четные на  9'''
+
+    return number * 10 if number % 2 == 0 else number * 9
+
